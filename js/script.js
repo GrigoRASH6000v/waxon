03f438725509d134c581c65e9b5b4bb5521c094f
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2019-10-31 08:57:54 +0300

    com-2-master

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var pagination = document.querySelectorAll(".circle");
+
+console.log(pagination)
+
+/*pagination.addEventListener('click', function(evt){
+evt.preventDefault();
+for(var i=0; i<=pagination.length; i++){
+	console.log(pagination[i]);
+}
+});*/
