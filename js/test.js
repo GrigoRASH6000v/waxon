b5560f019180cc460019eb279bd0259ad45ce32d
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2019-11-12 02:18:42 +0300

    com-5-master

--- js/test.js
+++ js/test.js
@@ -21,6 +21,49 @@ $(document).ready(function(){
     });
 });
 
-/*var btn=$('#playpause').css('background-image');
+var menuBtn = document.querySelector('.menu-icon');
+var menuList = document.querySelector('.menu-list');
+var coordinate=document.querySelector('.coordinate');
 
-console.log(btn);*/
+menuBtn.addEventListener('click', function(evt){
+    evt.preventDefault();
+
+    
+    if(menuBtn.classList.contains('close')){
+       menuList.style.display="none"; 
+       menuBtn.classList.remove('close');
+    }else{
+        menuList.style.display="block";
+         menuBtn.classList.toggle('close');
+    }
+
+})
+
+
+var messageBlock =document.querySelectorAll(".message-block");
+var messageBtnBack = document.querySelector('.back');
+var messageBtnForward = document.querySelector('.forward');
+
+var firstSlide=0;
+var lastSlide=2;
+
+
+
+messageBtnForward.addEventListener('click', function(){
+    if(messageBlock[lastSlide]!==messageBlock[messageBlock.length-1]){
+        messageBlock[lastSlide+1].style.display="block"
+        messageBlock[firstSlide].style.display="none"
+        firstSlide++;
+        lastSlide++;
+    };
+  
+});
+
+messageBtnBack.addEventListener('click', function(){
+    if(firstSlide!==0){
+        messageBlock[lastSlide].style.display="none"
+        messageBlock[firstSlide-1].style.display="block"
+        firstSlide--;
+        lastSlide--;
+    };
+});
